d1c3bf88c93102793e87af3b45e719d455e285d6
Linus Ericsson <linus.ericsson@agical.se>
2022-01-01 23:24:14 +0100

    update dependencies
    [org.clojure/clojure "1.10.3"] is available but we use "1.10.1"
    [org.jsoup/jsoup "1.14.3"] is available but we use "1.13.1"
    [com.icegreen/greenmail "1.6.5"] is available but we use "1.6.1"
    
    we update to 1.6.7, not 2.0.1
    [com.sun.mail/jakarta.mail "2.0.1"] is available but we use "1.6.5"
    
    update lein-cljfmt from 0.7.0 to 0.8.0 as well

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/forward/clojure-mail"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.jsoup/jsoup "1.13.1"] ;; for cleaning up messy html messages
-                 [com.sun.mail/jakarta.mail "1.6.5"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.jsoup/jsoup "1.14.3"] ;; for cleaning up messy html messages
+                 [com.sun.mail/jakarta.mail "1.6.7"]
                  [medley "1.3.0"]]
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :creds :gpg}]]
-  :plugins [[lein-cljfmt "0.7.0"]]
-  :profiles {:dev {:dependencies [[com.icegreen/greenmail "1.6.1"]]}})
+  :plugins [[lein-cljfmt "0.8.0"]]
+  :profiles {:dev {:dependencies [[com.icegreen/greenmail "1.6.5"]]}})
